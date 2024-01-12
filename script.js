f58e23728cec18ceebf7914ae30b7adb299a8767
RomanFadeev71 <roman-fadeev97@mail.ru>
2024-01-12 23:13:33 +0300

    ДЗ Java script не доделана подскажите плиз

---
+++ script.js
@@ -0,0 +1,44 @@
+alert(10)
+alert(20)
+let release = alert(`Выпуск первого айфона 29 июня 2007 года`);
+let creator = alert(`Брендан Эйх`);
+
+let a = 10;
+let b = 2;
+alert(a + b)
+alert(a - b)
+alert(a * b)
+alert(a / b)
+
+let c = 2;
+let d = 5;
+let result = c ** d;
+alert(result);
+
+let e = 9;
+let t = 2;
+let newresult = e / t;
+alert(newresult);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = Number(prompt(`Сколько вам лет?`));
+alert(26);
+
+let user = {
+    name: `Roman`,
+    age: 26,
+    isAdmin: true,
+}
+user[cityofresidence] = Moscow;
+alert(user[cityofresidence]);
+
+let info = prompt("Какую информацию хотите узнать о пользователе?")
+alert(user[info]);
